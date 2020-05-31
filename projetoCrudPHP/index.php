aa0b9f2948a0ec54f75973569ed5850516f61053
Aline Ferreira <63170961+Aline-Ferreira1980@users.noreply.github.com>
2020-05-31 17:42:31 -0300

    subindo

---
+++ projetoCrudPHP/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel ="stylesheet" href = "https://getbootstrap.com/docs/4.5/examples/sign-in/?">
+    <title>Boas Vindas</title>
+
+
+</head>
+<body>
+   <p> Bem vindo ao jogo dos 7 erros:</br>
+    Cadastre-se para continuar jogando!!!
+    </p>
+</body>
+</html>
